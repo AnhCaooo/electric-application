51f5fed1df2c4d43752822532a5bf44bf0d0114a
Anh Cao <anhcao4922@gmail.com>
2024-10-26 21:46:19 +0300

    include keycloak custom image and update architecture

--- diagram.py
+++ diagram.py
@@ -1,12 +1,16 @@
 from diagrams import Diagram, Cluster, Edge
 from diagrams.onprem.network import Nginx
 from diagrams.programming.language import NodeJS, Go, Swift
-from diagrams.onprem.database import MongoDB
+from diagrams.onprem.database import MongoDB, PostgreSQL
 from diagrams.onprem.container import Docker
 from diagrams.firebase.grow import Messaging
 from diagrams.programming.framework import React
+from diagrams.custom import Custom
 
 with Diagram("Electric Application Architecture", show=False):
+
+    
+    # Client services 
     with Cluster('Client platforms'):
         with Cluster('iOS'):
             ios = Swift('Sthor')
@@ -15,35 +19,40 @@ with Diagram("Electric Application Architecture", show=False):
     # Firebase Cloud Messaging 
     with Cluster('Firebase'):
         fcm = Messaging("Cloud Messaging")   
+    
     with Cluster('AWS EC2'):   
-        with Cluster('Web Server'):         
+        with Cluster('Web Server & Reverse proxy'):         
             nginx = Nginx("Nginx")
+        with Cluster("IAM service"):
+            # define keycloak data 
+            keycloak = Custom("Keycloak", "./assets/keycloak.png")
+
         # Database
-        with Cluster('Database/Collections'):
-            auth_db = MongoDB("Auth")
+        with Cluster('Mongodb'):
             user_db = MongoDB("User")
             device_token_db = MongoDB("Device Tokens")
-
+        
         # Define services                     
         with Cluster('API Gateway'):
             api_gateway = NodeJS("Doctor Strange")
         with Cluster('Backend services'):
-            auth = Go("Auth service")             
-            user = Go("User service")
             electric = Go("Electric service")
             notifications = Go("Notifications service")
+            
+        keycloak_db = PostgreSQL("Keycloak")
+        
             
     # Architecture
     ios >> nginx 
     react >> nginx
-    nginx >> Edge(color="darkgreen") << api_gateway 
+    nginx >> Edge(color="darkgreen") << keycloak 
+    keycloak >> keycloak_db
+    keycloak >> Edge(color="darkgreen", label="If authenticate successful") >> api_gateway 
     
-    api_gateway >> auth >> auth_db
-    api_gateway >> user >> user_db
     api_gateway >> electric >> user_db
     api_gateway >> notifications 
     
-    electric >> Edge(color="brown") << notifications
+    # electric >> Edge(color="brown") << notifications
     notifications >> device_token_db
     notifications >> fcm >> Edge(label="get device token") >> ios
         
